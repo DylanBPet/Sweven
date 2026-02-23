2f712dd5f93fac3f965223e2a813c48891d35822
DylanBPet <dylanpetroff13@gmail.com>
2026-02-22 23:48:07 -0500

    Added the animation and player movement

--- Sweven/Assets/Scripts/PlayerWalkingScript.cs
+++ Sweven/Assets/Scripts/PlayerWalkingScript.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerWalkingScript : MonoBehaviour
 {
+    public Rigidbody2D rigidBody;
+
+    public Animator animator;
+
+    public Vector2 input;
+
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("moveX", input.x);
+        animator.SetFloat("moveY", input.y);
+
+        transform.position += (Vector3) input * speed * Time.deltaTime;
     }
 }
